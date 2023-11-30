d42f86e465a6e02c0d3a0506d18d13206dd7dfcd
hannakaczynska <hannakacz13@gmail.com>
2023-11-30 18:23:58 +0100

    added

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+//Handlebars template
+//const expressHbs = require("express-handlebars");
+// app.engine(
+//   "hbs",
+//   expressHbs({
+//     layoutsDir: "views/layouts/",
+//     defaultLayout: "main-layout",
+//     extname: "hbs",
+//   })
+// );
+// app.set("view engine", "hbs");
+
+//PUG template
+// app.set("view engine", "pug");
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use("/admin", adminRoutes.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  res.status(404).render("404", { docTitle: "Not Found" });
+});
+
+app.listen(3000);
